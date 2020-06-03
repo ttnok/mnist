b5765da86f03b0490b2ad6a3876fe5b73d22413e
ttnok <16523139+ttnok@users.noreply.github.com>
2020-06-04 00:01:12 +0900

    update

--- docs/viewer/script.js
+++ docs/viewer/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const $ = selector => {
     return document.querySelector(selector);
 };
@@ -6,7 +8,7 @@ const $$ = selector => {
 };
 
 const canvas = $('canvas');
-const context = canvas.getContext('2d');
+const context = canvas.getContext('2d', {alpha: false});
 
 const imageData = context.createImageData(canvas.width, canvas.height);
 let data = imageData.data;  // : Uint8ClampedArray
@@ -17,17 +19,16 @@ const load_nth_image = n => {  // 1-based
     const offset = 16 + 28 * 28 * (n - 1);
 
     for (let i = 0; i < 28 * 28; i++) {
-        data[4 * i] = data_view.getUint8(offset + i);
-        data[4 * i + 1] = data_view.getUint8(offset + i);
-        data[4 * i + 2] = data_view.getUint8(offset + i);
-        data[4 * i + 3] = 255;
+        let d = data_view.getUint8(offset + i);
+        data[4 * i] = data[4 * i + 1] = data[4 * i + 2] = d;
     }
 
     context.putImageData(imageData, 0, 0);
 };
 
-const render = (n_start = 1, n_end = 10) => {
-    const n_images_in_each_page = 800;
+const n_images_in_each_page = 800;
+
+const build_pages = (n_start = 1, n_end = 10) => {
     const pages = [...Array(n_end - n_start + 1).keys()].map(p => p + n_start);
     const splitted = pages.reduce((splitted, n) => {
         if (n % n_images_in_each_page === 1) {
@@ -56,28 +57,39 @@ const render = (n_start = 1, n_end = 10) => {
         const images_elm = Array.from($$('.page > div')).slice(-1)[0];
 
         image_nums.forEach(n => {
-            load_nth_image(n);
-            canvas.toBlob(blob => {
-                const img = document.createElement('img');
-                const url = URL.createObjectURL(blob);
+            let img = document.createElement('img');
 
-                img.classList.add('figure');
-                img.src = url;
+            img.classList.add('figure');
+            img.id = `fig-${n}`;
 
-                images_elm.appendChild(img);
-            });
+            images_elm.appendChild(img);
         })
     })
 };
 
-const clear_outputs = () => {
+const render2 = async (n_start = 1, n_end = 10) => {
+    for (let n = n_start; n <= n_end; n++) {
+        load_nth_image(n);
+        let blob = await new Promise(resolve => canvas.toBlob(resolve));
+        let url = URL.createObjectURL(blob);
+
+        $(`#fig-${n}`).src = url;
+    }
+};
+
+const clear_outputs = () => {    
     $$('.page').forEach(page => page.remove());
 };
 
+
 const main = () => {
-    render(1, +$('#inputnumber > input').value);
+    build_pages(1, +$('#inputnumber > input').value);
+    render2(1, +$('#inputnumber > input').value);
 };
 
+/**
+ * Event Listners
+ */
 $('body').addEventListener('dragover', e => {
     e.stopPropagation();
     e.preventDefault();
@@ -104,5 +116,7 @@ $('body').addEventListener('drop', e => {
 $('#inputnumber > button').addEventListener('click', e => {
     const n_end = +$('#inputnumber > input').value;
     clear_outputs();
-    render(1, n_end);
+    build_pages(1, n_end);
+
+    render2(1, n_end);
 });
