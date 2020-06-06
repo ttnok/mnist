5009c95854457b481c3d77d8e67773774f605f5c
ttnok <16523139+ttnok@users.noreply.github.com>
2020-06-06 13:11:39 +0900

    refactored

--- docs/viewer/script.js
+++ docs/viewer/script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+/**
+ * global variables
+ */
+
 const $ = selector => {
     return document.querySelector(selector);
 };
@@ -11,48 +15,54 @@ const canvas = $('canvas');
 const context = canvas.getContext('2d', {alpha: false});
 
 let dataset = {
-    required: {
-        label: "train-labels-idx1-ubyte",
-        image: "train-images-idx3-ubyte"
+    label: {
+        filename: "train-labels-idx1-ubyte",
+        loaded: false,
+        dataview: null,
+        data: []
     },
-    loaded: {
-        label: false,
-        image: false
-    }
-};
-
-const imageData = context.createImageData(canvas.width, canvas.height);
-let data = imageData.data;  // : Uint8ClampedArray
-
-let data_view = {  // would be an instance of DataView
-    label: null,
-    image: null
-};
-
-let label = [];       // labels (1-based)
-
-const load_labels = () => {  // load ALL labels
-    const offset = 8;
-    const data_size = data_view.label.getUint32(4);
-    console.log(data_size);
-    for (let i = 0; i < data_size; i++) {
-        label[i + 1] = data_view.label.getUint8(offset + i);
-        // 1-based
-    }
-};
-
-const load_nth_image = n => {  // 1-based
-    const offset = 16 + 28 * 28 * (n - 1);
-
-    for (let i = 0; i < 28 * 28; i++) {
-        let d = data_view.image.getUint8(offset + i);
-        data[4 * i] = data[4 * i + 1] = data[4 * i + 2] = d;
-    }
-
-    context.putImageData(imageData, 0, 0);
+    image: {
+        filename: "train-images-idx3-ubyte",
+        loaded: false,
+        dataview: null
+    },
 };
 
 const n_images_in_each_page = 500;
+
+
+/**
+ * funcitons
+ */
+
+const load_labels = () => {  // load ALL labels
+    const offset = 8;
+    const data_size = dataset.label.dataview.getUint32(4);
+
+    for (let i = 0; i < data_size; i++) {
+        // 1-based
+        dataset.label.data[i + 1] = dataset.label.dataview.getUint8(offset + i);
+    }
+};
+
+const load_nth_image = (() => {
+    const imageData = context.createImageData(canvas.width, canvas.height);
+    let imagedata_on_canvas = imageData.data;  // : Uint8ClampedArray
+
+    return n => {  // n: 1-based
+        const offset = 16 + 28 * 28 * (n - 1);
+
+        for (let i = 0; i < 28 * 28; i++) {
+            let d = dataset.image.dataview.getUint8(offset + i);
+            imagedata_on_canvas[4 * i]
+                = imagedata_on_canvas[4 * i + 1]
+                = imagedata_on_canvas[4 * i + 2]
+                = d;
+        }
+
+        context.putImageData(imageData, 0, 0);
+    }
+})();
 
 const build_pages = (n_start = 1, n_end = 10) => {
     // create array of arrays from [n_start, ... , n_end]
@@ -98,8 +108,6 @@ const build_pages = (n_start = 1, n_end = 10) => {
 
             // <img>
             let img = document.createElement('img');
-            // img.classList.add('figure');
-            // img.id = `fig-${n}`;
 
             // <figcaption>
             let figcaption = document.createElement('figcaption');
@@ -122,8 +130,7 @@ const render = async (n_start = 1, n_end = 10) => {
         let url = URL.createObjectURL(blob);
 
         $(`#fig-${n} > img`).src = url;
-        // $(`#fig-${n} > figcaption`).textContent = `${(''+n).padStart(5, '0')} - ${label[n]}`;
-        $(`#fig-${n} > figcaption`).textContent = `${label[n]}`;
+        $(`#fig-${n} > figcaption`).textContent = `${dataset.label.data[n]}`;
     }
 };
 
@@ -139,6 +146,7 @@ const main = () => {
 /**
  * Event Listners
  */
+
 $('body').addEventListener('dragover', e => {
     e.stopPropagation();
     e.preventDefault();
@@ -155,14 +163,18 @@ $('body').addEventListener('drop', e => {
         let view = new DataView(reader.result);
 
         if (view.getUint32(0) === 2049) { // label
-            data_view.label = view;
+            dataset.label.dataview = view;
             $('#label_file').classList.add('loaded');
             load_labels();
         } else if (view.getUint32(0) === 2051) { // image
-            data_view.image = view;
+            dataset.image.dataview = view;
             $('#image_file').classList.add('loaded');
         } else {
             console.log('error: file collapsed.');
+        }
+
+        if (dataset.label.loaded && dataset.image.loaded) {
+            $('#inputnumber > button').removeAttribute('disabled');
         }
     });
 
@@ -171,16 +183,16 @@ $('body').addEventListener('drop', e => {
 
     ['label', 'image']
         .filter(key => {
-            return !dataset.loaded[key];
+            return !dataset[key].loaded;
         })
-        .forEach(key => {
-            let required_filename = dataset.required[key];
+        ?.forEach(key => {
+            let required_filename = dataset[key].filename;
             dropped_files.forEach(async f => {
                 if (f.name === required_filename) {
                     const reader = new FileReader();
                     reader.addEventListener('load', load_handler(reader));
                     reader.readAsArrayBuffer(f);
-                    dataset.loaded[key] = true;    
+                    dataset[key].loaded = true;    
                 }
             })
         });
